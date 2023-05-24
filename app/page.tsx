030ebbf2fa7e60e109e9622d76a15166aa402878
ddammo55 <ddammo55@naver.com>
2023-05-24 19:01:30 +0900

    버전2

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   console.log(os.hostname());
   return (
     <>
-      <h1>홈페이지다</h1>
+      <h1>홈페이지다 버전2</h1>
       <Counter />
       <Image src="https://images.unsplash.com/photo-1684619419581-39800cb71285?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=400&q=60" alt="shop"
       width={400}
